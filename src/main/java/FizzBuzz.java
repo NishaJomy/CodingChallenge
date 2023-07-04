ac5a3c73aaed0aee8e4713ee744a73faa363b20b
Nisha George <nishajomy.2007@gmail.com>
2023-07-03 22:08:41 -0500

    first commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        System.out.println(PrintFizzBuzz(2345) );
+
+
+    }
+
+    public static Integer PrintFizzBuzz(int num) {
+    int rev;
+    int temp;
+    for(int i=0;i<=3;i++){
+
+          temp =  i%10
+
+
+
+
+        }
+    }
+      return rev;
+    }
+}
+
